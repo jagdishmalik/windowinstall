149c1780ef934e75ce972f344d80563e2589b768
jagdishmalik <jagdishmalik24x7@gmail.com>
2023-02-18 22:27:18 +0530

    try once

--- createarrow.js
+++ createarrow.js
@@ -6,3 +6,56 @@ const arrown = (a, b) => {
 
 let x = arrown(23, 89);
 console.log(x);
+
+let result;
+result = '4' - '2'; 
+
+console.log(result); // 2
+
+result = '4' - 2;
+
+console.log(result); // 2
+
+result = '4' * 2;
+
+console.log(result); // 8
+
+result = '4' / 2;
+
+console.log(result); // 2
+result = 'hello' - 'world';
+
+console.log(result); // NaN
+
+result = '4' - 'hello';
+
+console.log(result); // NaN
+
+result = Number(null);
+
+console.log(result);  // 0
+
+ result = Number(' ')
+
+console.log(result);  // 0
+
+
+
+
+result = parseInt('20.01');
+
+console.log(result); // 20
+
+result = parseFloat('20.01');
+
+console.log(result); // 20.01
+
+result = +'20.01';
+
+console.log(result); // 20.01
+
+result = Math.floor('20.01');
+
+console.log(result); // 20
+
+
